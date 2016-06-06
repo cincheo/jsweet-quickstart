280f6ae530562b413a49a8c3cd2cc8f689a729e5
Renaud Pawlak <renaud.pawlak@gmail.com>
2016-06-06 21:00:23 +0200

    to version 1.1.1

--- src/main/java/quickstart/QuickStart.java
+++ src/main/java/quickstart/QuickStart.java
@@ -1,11 +1,56 @@
 package quickstart;
 
-import static jsweet.dom.Globals.alert;
+import java.util.function.BinaryOperator;
 
+import jsweet.lang.Array;
+
+/**
+ * This example uses a Java map to store and display complementary colors. It
+ * demonstrates that Java APIs and JavaScript APIs can be used together to build
+ * programs.
+ */
 public class QuickStart {
 
 	public static void main(String[] args) {
-		alert("It works!");
+		new WrongOverloadConstructorWithVarargs<Double>().test(2);
+		new WrongOverloadConstructorWithVarargs<Double>(2, (a, b) -> a + b, (a, b) -> a - b).test(2, (a, b) -> a + b,
+				(a, b) -> a - b);
+		// same but without varargs
+		new WrongOverloadConstructorWithVarargs<Double>(2).test(2);
+		// $export("trace", trace);
+		System.out.println(WrongOverloadConstructorWithVarargs.trace);
+	}
+
+	public static class WrongOverloadConstructorWithVarargs<T> {
+
+		static Array<String> trace = new Array<>();
+
+		public WrongOverloadConstructorWithVarargs() {
+		}
+
+		@SafeVarargs
+		public WrongOverloadConstructorWithVarargs(int defaultLevel, BinaryOperator<T>... precedence) {
+			this(null, defaultLevel, precedence);
+			trace.push("c2");
+		}
+
+		@SafeVarargs
+		private WrongOverloadConstructorWithVarargs(WrongOverloadConstructorWithVarargs<T> parent, int defaultLevel,
+				BinaryOperator<T>... precedence) {
+			trace.push("c1");
+		}
+
+		@SafeVarargs
+		public final void test(int defaultLevel, BinaryOperator<T>... precedence) {
+			test(null, defaultLevel, precedence);
+			trace.push("t2");
+		}
+
+		private void test(WrongOverloadConstructorWithVarargs<T> parent, int defaultLevel,
+				@SuppressWarnings("unchecked") BinaryOperator<T>... precedence) {
+			trace.push("t1");
+		}
+
 	}
 
 }
