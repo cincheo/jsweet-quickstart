2f6902d8a83b84f525d9b9d04d447555b71a7d77
Renaud Pawlak <renaud.pawlak@gmail.com>
2016-08-31 16:55:57 +0200

    added jQuery use + some comments

--- src/main/java/quickstart/QuickStart.java
+++ src/main/java/quickstart/QuickStart.java
@@ -1,11 +1,30 @@
 package quickstart;
 
 import static jsweet.dom.Globals.alert;
+import static def.jquery.Globals.$;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jsweet.lang.Array;
+
+/**
+ * This class is used within the webapp/index.html file.
+ */
 public class QuickStart {
 
 	public static void main(String[] args) {
-		alert("It works!");
+		// you can use regular Java API thanks to the J4TS candy
+		List<String> l = new ArrayList<>();
+		l.add("Hello");
+		l.add("world");
+		// and you can also use regular JavaScript APIs
+		Array<String> a = new Array<>();
+		a.push("Hello", "world");
+		// use of jQuery with the jQuery candy
+		$("#target").text(l.toString());
+		// use of the JavaScript DOM API
+		alert(a.toString());
 	}
 
 }
